46559ca83d099a135582c81cd989213b9c372164
cee breezy <djmisscbrown@hotmail.com>
2022-01-29 11:29:38 +0000

    Updated event listeners to buttons

--- script.js
+++ script.js
@@ -99,17 +99,10 @@ function createSearchHistoryButtons(){
 button.addEventListener("click", () => {
   const currentVal = search.value;
 
-  ft.getCurrent(currentVal).then((data) => {
-    //call a UI method//
-    ui.populateUI(data);
-    //call saveToLS
-    ui.saveToLS(data);
-  });
+  // button.addEventListener("click", () => {
+  //   const History = previousSearchHistory;
+
+  getCurrentSearchCoords(currentVal);
 });
 
-//event listener for local storage
-
-window.addEventListener("DOMContentLoaded", () => {
-  const dataSaved = ui.getFromLS();
-  ui.populateUI(dataSaved);
-});
+createSearchHistoryButtons();
